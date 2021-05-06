e33887dba22f136996c94f7740531711a6444ffb
Alex Siroli <alex.siroli@studio.unibo.it>
2021-05-06 23:43:48 +0200

    Aggiunta interfaccia e classe generica di un manager che gestisce gli elementi

---
+++ AlexSiroli/CSharp/Manager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace AlexSiroli
+{
+
+	/// Immutable implementation of a manager.
+	/// @param <X> type of elements it manages. </param>
+	public class Manager<X> : IManager<X>
+	{
+
+		private readonly ISet<X> elements;
+
+        public Manager()
+        {
+            this.elements = new HashSet<X>();
+        }
+
+		/// {@inheritDoc}
+		public void Add(X elem)
+		{
+			this.elements.Add(elem);
+		}
+
+		/// {@inheritDoc}
+		public void Remove(X elem)
+		{
+			this.elements.Remove(elem);
+		}
+
+		public ISet<X> Elements
+		{
+			get
+			{
+				return this.elements;
+			}
+		}
+    }
+}
