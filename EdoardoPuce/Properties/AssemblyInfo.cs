acebe9e939f36c48b7d4375b2b0799a9a3c20f08
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-05-06 12:44:54 +0200

    Aggiunta repository personale e Interfaccia IDay

---
+++ EdoardoPuce/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EdoardoPuce")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("EdoardoPuce")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eec6e974-fbf8-4881-92a0-9b261a4b1f43")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EdoardoPuce/src/IDay.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdoardoPuce.src
+{
+	/// <summary>
+	/// The Days interface models the object day.
+	/// It defines the standard methods for manage the days of Calendar
+	/// </summary>
+	public interface IDay
+    {
+		/// <summary>
+		/// Can be used to get the number of month of the day. </summary>
+		/// <returns> the number of the day of the month </returns>
+		int GetNumber { get; }
+
+		/// <summary>
+		/// Can be used to get the day of the week. </summary>
+		/// <returns> the day of the week 
+		///  </returns>
+		int GetDayOfTheWeek { get; }
+
+
+		/// <summary>
+		/// Can be used to get the name of the day. </summary>
+		/// <returns> the name of the day  </returns>
+		string GetName { get; }
+
+		/// <summary>
+		/// Can be used to get the Month of the day. </summary>
+		/// <returns> the name of the Month  </returns>
+		string GetMonth { get; }
+
+		/// <summary>
+		/// Can be used to get the number of the Month of the day. </summary>
+		/// <returns> the number of the Month  </returns>
+		int GetMonthNumber { get; }
+
+		/// <summary>
+		/// Can be used to get the Year of the day. </summary>
+		/// <returns> the number of the Year  </returns>
+		int GetYear { get; }
+
+		/// <summary>
+		/// Can be used to get the list of the events of the day. </summary>
+		/// <returns> List : the list of event of the day </returns>
+		List<String> GetEvents { get; }
+
+
+		/// <summary>
+		/// Can be used to get the list of the daily events of the day. </summary>
+		/// <returns> List : the list of the daily event of the day </returns>
+		List<String> GetDailyEvents { get; }
+	}
+}
